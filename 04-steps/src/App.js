336010884944801642501cbb73ce8bf2311c048a
Darryl Le Roux <devdarryl@outlook.com>
2024-03-05 21:19:07 +0200

    Event handling

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,5 +1,43 @@
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
 function App() {
-  return <div>Hello React</div>;
+  const step = 1;
+
+  const handlePrevious = () => alert('previous');
+  const handleNext = () => alert('next');
+
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
+
+      <p className='message'>
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className='buttons'>
+        <button
+          style={{ backgroundColor: '#7950F2', color: '#FFF' }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: '#7950F2', color: '#FFF' }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
